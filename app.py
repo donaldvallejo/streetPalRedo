2d30369450409578d3487a327de5eb5ef6875482
Donald Vallejo <donny.vallejo@gmail.com>
2019-10-18 15:41:33 -0700

    last push before presentation

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask_googlemaps import GoogleMaps
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/streetPal')
 client = MongoClient(host=f"{host}?retryWrites=false")
 db = client.get_default_database()
-service = db.services
+services = db.streetPal
 comments = db.comments
 app = Flask(__name__)
 
@@ -19,32 +19,35 @@ maps = GoogleMaps(app, key="AIzaSyChhJt9bRZvlbMol6VvdQNKwR1BrvV9kx8")
 @app.route('/')
 def services_index():
   """Show all services"""
-  return render_template('streetPals_index.html', services=service)
 
-@app.route('/services/new')
+  return render_template('streetPals_index.html', services=services)
+
+@app.route('/streetPals/new')
 def services_new():
   """Create services"""
-  return render_template('streetPals_new.html', service = {}, title='New services')
+  return render_template('streetPals_new.html', services = {}, title='New services')
 
 @app.route('/services', methods=['POST'])
 def services_submit():
   """Submit services to database"""
 
   service = {
-    'name': request.form.get('Name'),
-    'description': request.form.get('Description'),
-    'address': request.form.get('Address'),
-    'number': request.form.get('Number'),
-    'hours': request.form.get('Hours'),
+    'name': request.form.get('name'),
+    'description': request.form.get('description'),
+    'address': request.form.get('address'),
+    'number': request.form.get('number'),
+    'hours': request.form.get('hours'),
     'url': request.form.get('url'),
     'services': request.form.get('Services')
   }
 
 
-  service = services.insert_one(service).inserted_id
-  print(service)
+
+  service_id = services.insert_one(service).inserted_id
+  print("="*30)
+  print(service_id)
   # print("Check to see if Data is even happening \n", service_id, service)
-  return redirect(url_for('services_show', service=service))
+  return redirect(url_for('services_show', service_id=service_id))
 
 @app.route('/services/<service_id>')
 def services_show(service_id):
@@ -52,13 +55,24 @@ def services_show(service_id):
     service = services.find_one({'_id': ObjectId(service_id)})
     service_comments = comments.find({'service_id': ObjectId(service_id)})
     print(service)
-    return render_template('services_show.html', service=service, comments=service_comments)
+    return render_template('streetPals_show.html', service=service, comments=service_comments)
 
 @app.route('/services/<service_id>/edit')
 def services_edit(service_id):
     """Show the edit form for a service."""
     service = services.find_one({'_id': ObjectId(service_id)})
-    return render_template('services_edit.html', service=service, title='Edit service')
+    service_update = {
+    'name': request.form.get('name'),
+    'description': request.form.get('description'),
+    'address': request.form.get('address'),
+    'number': request.form.get('number'),
+    'hours': request.form.get('hours'),
+    'url': request.form.get('url'),
+    'services': request.form.get('Services')
+    }
+    #services.update_one({"_id":service_id}, {"$set": service_update})
+    services.update_one({"_id":service_id, **service_update})
+    return render_template('streetPals_edit.html', service=service, title='Edit service')
 
 '''
 @app.route('/search', methods=['POST'])
@@ -112,14 +126,14 @@ def comments_new():
         'service_id': ObjectId(request.form.get('service_id'))
     }
     comment_id = comments.insert_one(comment).inserted_id
-    return redirect(url_for('services_show', service_id=request.form.get('service_id')))
+    return redirect(url_for('services_show', comment_id=comment_id, service_id=request.form.get('service_id')))
 
 @app.route('/services/comments/<comment_id>', methods=['POST'])
 def comments_delete(comment_id):
     """Action to delete a comment."""
     comment = comments.find_one({'_id': ObjectId(comment_id)})
     comments.delete_one({'_id': ObjectId(comment_id)})
-    return redirect(url_for('services_show', service_id=comment.get('service_id')))
+    return redirect(url_for('services_show', comment_id=comment._id, service_id=comment.get('service_id')))
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
